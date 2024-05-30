5ecc58cce186898702d8d06adba609c110edca1e
Balazs Horvath <acsipont@gmail.com>
2024-05-30 08:59:23 +0200

    awoo
    Signed-off-by: Balazs Horvath <acsipont@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@ use serde_json::Value;
 use std::fs;
 use std::path::Path;
 use std::io::Write;
+use walkdir::WalkDir;
 
 fn main() {
     let directory_path = "E:/projects/yiff_toolkit/ponyxl_loras";
-    let paths = fs::read_dir(directory_path).unwrap();
-    for path in paths {
-        let path = path.unwrap().path();
-        if path.extension().unwrap() == "json" {
+    for entry in WalkDir::new(directory_path) {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        if path.extension().unwrap_or_default() == "json" {
             if let Err(e) = format_json_file(&path) {
                 println!("Failed to format {}: {}", path.display(), e);
             }
